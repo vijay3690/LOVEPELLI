33ccfb97f42678674fe70c1ee72bb13d1282fb26
vijay <vijaykumar.chikkam@weddysoft.com>
2025-08-26 17:28:03 +0530

    update this files

---
+++ src/pages/otpsigns/gmail.jsx
@@ -0,0 +1,53 @@
+import React,{useState} from 'react'
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import { Navigate } from 'react-router-dom';
+
+
+
+const CLIENT_ID = "568205557405-j4j911ckb6qo1ckgspto0e3idlb9ffia.apps.googleusercontent.com"; // Replace with your actual client ID
+
+const Glogin = () => {
+  
+  const [auth, setAuth] = useState(false);
+  
+  if (auth) {
+    return <Navigate to="/homefour" replace />;
+  }
+  
+
+    const handleSuccess = (response) => {
+        console.log("Login Success:", response);
+        setAuth(true); // Set auth to true on successful login
+      };
+    
+      const handleFailure = (error) => {
+        console.error("Login Failed:", error);
+      };
+
+ return (
+    <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <div className="flex justify-center items-center ">
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+      
+          <div style={{ width: '223px', borderRadius: '32px', overflow: 'hidden', boxShadow: '0 2px 12px rgba(0,0,0,0.08)' }}>
+            <GoogleLogin
+              onSuccess={handleSuccess}
+              onError={handleFailure}
+              theme="filled_white"
+              size="large"
+              shape="pill"
+              logo_alignment="left"
+            />
+          </div>
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
+
+
+export default Glogin;
