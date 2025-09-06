1375b5701a2ce6460a8fe8a60def603ea89d7334
vijay3690 <vijaygoru666@gmail.com>
2025-09-06 20:26:19 +0530

    Update registerconstants.js

--- src/pages/registration/registerconstants.js
+++ src/pages/registration/registerconstants.js
@@ -1,18 +1,3 @@
 
-export const BASE_API = "http://localhost:5103/api/BasicDetails";
-export const BASE_URL = "http://localhost:5103/api/PersonalDetails";  
-export const BASE_APIURL = "http://localhost:5103/api/ProfessionalDetails";
-export const BASE_APIURLUSER = "http://localhost:5103/api/Users";
-export const BASE_APIURLPROFILE = "http://localhost:5103/api/UserProfile";
-
-// export const religionMap = {
-//   1: "Hindu",
-//   2: "Muslim",
-//   3: "Christian",
-//   4: "Sikh",
-//   5: "Parsi",
-//   6: "Buddhist",
-//   7: "Inter-Religion",
-//   8: "Jain"
-// };
+export const BASE_API = "http://localhost:5103";
 
