59f53151619a60cbee9585c9dd0b62a9f2fd11c2
vijay3690 <vijaygoru666@gmail.com>
2025-09-07 12:00:54 +0530

    Update App.jsx
    Got it !You want route protection:
    
    If not authenticated → redirect to /login.
    
    If authenticated → allow access to private pages (/about, /members, etc.).
    
    Some routes like /login, /signup, /register, /logout should always remain public.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import 'swiper/css';
 
 import ScrollToTop from './component/layout/scrolltop';
@@ -7,7 +7,7 @@ import ActivityPage from "./pages/activity";
 import CommunityPage from "./pages/community";
 import ContactUs from "./pages/contact";
 import ErrorPage from "./pages/errorpage";
-import GroupPage from "./pages/group";
+import GroupPage from "./pages/grouppage";
 import GroupDetails from "./pages/groupsingle";
 import LogIn from "./pages/authentication/login";
 import MemberDetails from "./pages/member-single";
@@ -17,49 +17,44 @@ import Policy from "./pages/policy";
 import HomePageFour from "./pages/homepages/homefour";
 import HomePage from "./pages/homepages/home";
 import Logout from "./pages/authentication/logout";
-import SignUp from "./pages/otpsigns/signup";
+import SignUp from "./pages/otp-signs/signup";
 import Register from "./pages/registration/register";
 
-
-
+import ProtectedRoute from "/src/protectedroute";
 
 function App() {
-
-
 	return (
 		<div className="App">
 			<BrowserRouter>
 				<ScrollToTop />
 				<Routes>
-				    <Route path="/" element={<HomePage/>}/>
-				    <Route path="homefour" element={<HomePageFour />} />
-					<Route path="about" element={<AboutPage />} />
-					<Route path="membership" element={<MembershipPage />} />
-					<Route path="*" element={<ErrorPage />} />
-					<Route path="community" element={<CommunityPage />} />
-					<Route path="group" element={<GroupPage />} />
-					<Route path="group-single" element={<GroupDetails />} />
-					<Route path="members" element={<MembersPage />} />
-					<Route path="activity" element={<ActivityPage />} />
-				    <Route path="contact" element={<ContactUs />} />
-					<Route path="login" element={<LogIn />} />
+					{/* Public Routes */}
+					<Route path="/login" element={<LogIn />} />
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/register" element={<Register />} />
 					<Route path="/logout" element={<Logout />} />
-					<Route path="member-single" element={<MemberDetails />} />
-					<Route path="policy" element={<Policy />} />
-					<Route path="signup" element={<SignUp />} />
-	    	        <Route path="register" element={<Register />} />
+
+					{/* Protected Routes */}
+					<Route path="/" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
+					<Route path="/homefour" element={<ProtectedRoute><HomePageFour /></ProtectedRoute>} />
+					<Route path="/about" element={<ProtectedRoute><AboutPage /></ProtectedRoute>} />
+					<Route path="/membership" element={<ProtectedRoute><MembershipPage /></ProtectedRoute>} />
+					<Route path="/community" element={<ProtectedRoute><CommunityPage /></ProtectedRoute>} />
+					<Route path="/grouppage" element={<ProtectedRoute><GroupPage /></ProtectedRoute>} />
+					<Route path="/group-single" element={<ProtectedRoute><GroupDetails /></ProtectedRoute>} />
+					<Route path="/members" element={<ProtectedRoute><MembersPage /></ProtectedRoute>} />
+					<Route path="/activity" element={<ProtectedRoute><ActivityPage /></ProtectedRoute>} />
+					<Route path="/contact" element={<ProtectedRoute><ContactUs /></ProtectedRoute>} />
+					<Route path="/member-single" element={<ProtectedRoute><MemberDetails /></ProtectedRoute>} />
+					<Route path="/policy" element={<ProtectedRoute><Policy /></ProtectedRoute>} />
+					<Route path="errorpage" element={<ProtectedRoute><ErrorPage/></ProtectedRoute>}/>
+
+					{/* Catch-all → redirect */}
+					<Route path="*" element={<Navigate to="/login" replace />} />
 				</Routes>
 			</BrowserRouter>
-
-			
-			
 		</div>
 	);
 }
 
 export default App;
-
-
-
-
-
