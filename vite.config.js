adc7ad86de7cb0aa92a4fe8fa3aee39d198f6f23
vijay3690 <vijaygoru666@gmail.com>
2025-09-13 19:45:00 +0530

    update this file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // default output folder
+  },
+  server: {
+    port: 5173, // local dev server port
+  }
 });
