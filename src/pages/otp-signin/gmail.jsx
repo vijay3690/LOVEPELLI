df1aa2df85cc32f49f39e5fd1b8a57dfe65670f8
vijay3690 <vijaygoru666@gmail.com>
2025-09-14 16:02:13 +0530

    update google authentication client id

--- src/pages/otp-signin/gmail.jsx
+++ src/pages/otp-signin/gmail.jsx
@@ -1,45 +1,39 @@
-import React from "react";
+import React,{useState} from 'react'
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from 'react-router-dom';
 
-const CLIENT_ID = "447299056493-p2jsdgap2cnr7la807qmu4g6qi85mm2k.apps.googleusercontent.com"
-  // "447299056493-b2ormt215f6hqvbjmrla1jv4ft0d0ue4.apps.googleusercontent.com"; // vercel-github
+
+
+const CLIENT_ID = "447299056493-p2jsdgap2cnr7la807qmu4g6qi85mm2k.apps.googleusercontent.com"; // Replace with your actual client ID
 
 const Glogin = () => {
-  const navigate = useNavigate();
+  
+  const [auth, setAuth] = useState(false);
+  
+  if (auth) {
+    return <Navigate to="/homefour" replace />;
+  }
+  
 
-  const handleSuccess = (credentialResponse) => {
-    console.log("Login Success:", credentialResponse);
+    const handleSuccess = (response) => {
+        console.log("Login Success:", response);
+        setAuth(true); // Set auth to true on successful login
+      };
+    
+      const handleFailure = (error) => {
+        console.error("Login Failed:", error);
+      };
 
-    // Save user data if needed
-    localStorage.setItem("googleUser", JSON.stringify(credentialResponse));
-
-    // Redirect to homefour
-    navigate("/");
-  };
-
-  const handleFailure = (error) => {
-    console.error("Login Failed:", error);
-  };
-
-  return (
+ return (
     <GoogleOAuthProvider clientId={CLIENT_ID}>
-      <div className="flex justify-center items-center">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <div
-            style={{
-              width: "223px",
-              borderRadius: "32px",
-              overflow: "hidden",
-              boxShadow: "0 2px 12px rgba(0,0,0,0.08)",
-            }}
-          >
+      <div className="flex justify-center items-center ">
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}>
+      
+          <div style={{ width: '223px', borderRadius: '32px', overflow: 'hidden', boxShadow: '0 2px 12px rgba(0,0,0,0.08)' }}>
             <GoogleLogin
               onSuccess={handleSuccess}
               onError={handleFailure}
@@ -54,5 +48,6 @@ const Glogin = () => {
     </GoogleOAuthProvider>
   );
 };
+
 
 export default Glogin;
