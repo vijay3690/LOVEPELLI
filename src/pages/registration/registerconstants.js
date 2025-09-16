46158549747ab48ed3090445ddf1833e201c26eb
vijay3690 <vijaygoru666@gmail.com>
2025-09-16 22:25:43 +0530

    update base_api url

--- src/pages/registration/registerconstants.js
+++ src/pages/registration/registerconstants.js
@@ -1,3 +1,5 @@
 
-export const BASE_API = "http://localhost:5103";
+// export const BASE_API = "http://localhost:5103";
+
+export const BASE_API = "https://lovepelliapi-gdcmb2ezcvcmedew.eastus2-01.azurewebsites.net"
 
