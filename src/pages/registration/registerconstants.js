ddfb7d00127e9f6935de0ac66fd296758438bb57
vijay3690 <vijaygoru666@gmail.com>
2025-09-16 22:21:17 +0530

    update the base _api

--- src/pages/registration/registerconstants.js
+++ src/pages/registration/registerconstants.js
@@ -1,5 +1,4 @@
 
 // export const BASE_API = "http://localhost:5103";
 
-export const BASE_API = "https://lovepelliapi-gdcmb2ezcvcmedew.eastus2-01.azurewebsites.net"
-
+export const BASE_API = "https://lovepelliapi-gdcmb2ezcvcmedew.eastus2-01.azurewebsites.net";
