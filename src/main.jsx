6d4400c80fdd3aafbeac1b2bdd04a3b424d8f744
vijay3690 <vijaygoru666@gmail.com>
2025-11-13 12:59:07 +0530

    GoogleOAuthProvider  i sremoved in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -8,17 +8,13 @@ import '././assets/css/icofont.min.css';
 import '././assets/css/all.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-      <GoogleOAuthProvider clientId="116674436581-u7228uk99lgttf51r2km6iu2vsn0ump2.apps.googleusercontent.com">
           <App />
-    </GoogleOAuthProvider>
- 
   </React.StrictMode>
 );
 
