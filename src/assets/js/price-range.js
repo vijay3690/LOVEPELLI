c4ff63868a95d3d7dc0d33a015eca0d2560930c5
vijay3690 <vijaygoru666@gmail.com>
2025-12-24 16:01:50 +0530

    delete this un wanted folder assests in src

--- src/assets/js/price-range.js
+++
@@ -1,6 +0,0 @@
-var slider = document.getElementById("sidebarprice");
-var output = document.getElementById("sidebarpricevalu");
-output.innerHTML = slider.value;
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
